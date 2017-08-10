dbf43195aac6e0a8fc9f1cc805fd03423ff15a2a
secilozdemiroglu <30806535+secilozdemiroglu@users.noreply.github.com>
2017-08-10 08:54:06 +0300

    Update Vegetable.java

--- 20170809/Mahsuller/Vegetable.java
+++ 20170809/Mahsuller/Vegetable.java
@@ -1,21 +1,14 @@
-
 public  class Vegetable extends PlantIMPL {
 	
-	public String veggiename;
+	public String veggiename;	
 	
-	
-	public String getName()
-	{
-		return veggiename; 
-			
+	public String getName()	{
+		return veggiename; 			
 	}
-
 	public String getVeggiename() {
 		return veggiename;
 	}
-
 	public void setVeggiename(String veggiename) {
 		this.veggiename = veggiename;
 	}
-
 }
