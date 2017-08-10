1719bab867473e4dca83a3367ea49694a42d15c2
secilozdemiroglu <30806535+secilozdemiroglu@users.noreply.github.com>
2017-08-10 08:52:43 +0300

    Update MainClass.java

--- 20170809/Mahsuller/MainClass.java
+++ 20170809/Mahsuller/MainClass.java
@@ -5,25 +5,16 @@ public class MainClass {
 	{
 		PlantIMPL impl = new PlantIMPL();
 		Fruit meyve = new Fruit();
-		GreenApple yesilelma = new GreenApple();
-		
+		GreenApple yesilelma = new GreenApple();		
 		Tomatoes Domates = new Tomatoes();
-		GoldenApple SarýElma = new GoldenApple();
-		
-		RedApple.Peel();
-		
-		System.out.println(impl.isColored()+"\n"); 
-		
-		System.out.println(meyve.getName());
-	
+		GoldenApple SarýElma = new GoldenApple();		
+		RedApple.Peel();		
+		System.out.println(impl.isColored()+"\n"); 		
+		System.out.println(meyve.getName());	
 		System.out.println(yesilelma.getTaste());
 		System.out.println(yesilelma.getName()+"\n");
-	
-
-		System.out.println(Domates.getSeedCount()+"\n" );
-	
+		System.out.println(Domates.getSeedCount()+"\n" );	
 		System.out.println(SarýElma.getName());
 		System.out.println(SarýElma.getTaste());		
 	}
-
 }
