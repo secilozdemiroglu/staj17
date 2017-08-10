05000cbbb4bd31747eb35684f299bb6878549391
secilozdemiroglu <30806535+secilozdemiroglu@users.noreply.github.com>
2017-08-10 08:55:04 +0300

    Update PlantIMPL.java

--- 20170809/Mahsuller/PlantIMPL.java
+++ 20170809/Mahsuller/PlantIMPL.java
@@ -1,15 +1,11 @@
-
 public class PlantIMPL implements Plant{
-
 	public boolean isColored(){
 		System.out.println("Just eat it!");
 		return true;
-	}
-	
+	}	
 	@Override
 	public String getName() {
 		// TODO Auto-generated method stub
 		return null;
 	}
-
 }
