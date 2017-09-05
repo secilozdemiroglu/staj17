19575bb02db1ef3dbe3b922d3b1a378bb5f5e230
secilozdemiroglu <30806535+secilozdemiroglu@users.noreply.github.com>
2017-09-05 11:34:24 +0300

    Update Main.java

--- Air&Earth/Main.java
+++ Air&Earth/Main.java
@@ -1,14 +1,29 @@
 
 public class Main {
 
+	public static void detectElement(Object o) {
+		switch (o.getClass().getName()) {
+		case "Air":
+			System.out.println("Take a deep breath");
+			break;
+		case "Earth":
+			System.out.println("Walk of life");
+			break;
+
+		default:
+			System.out.println("Why do we exist?");
+			break;
+		}
+
+	}
+
 	public static void main(String[] args) {
 		Air a = new Air();
-		Earth e= new Earth();
-	System.out.println(a.detectElement("o"));	
-	System.out.println(e.detectElement("o"));
-	System.out.println(a.detectElement("p"));
-	System.out.println(e.detectElement("s"));
+		Earth e = new Earth();
+
+		detectElement(a);
+		detectElement(e);
+	    detectElement(args);
 	}
 
 }
-
